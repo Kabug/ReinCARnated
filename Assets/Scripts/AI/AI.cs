f4c875d2a2f3d46e344eb02492a8b3655c096d88
AndyZhou1998 <47257849+AndyZhou1998@users.noreply.github.com>
2023-02-14 23:05:54 -0800

    Basic Ai sphere follower

--- Assets/Scripts/AI/AI.cs
+++ Assets/Scripts/AI/AI.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
+    [SerializeField] private Transform movePositionTransform;
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake(){
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        navMeshAgent.destination = movePositionTransform.position;
     }
 }
