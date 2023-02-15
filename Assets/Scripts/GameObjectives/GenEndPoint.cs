4e69ef01fda9cd47d6bbe1b656ffa249957b9a7c
gaganheer <gagandeep_heer29@hotmail.com>
2023-02-14 23:25:45 -0800

    more stuff

--- Assets/Scripts/GameObjectives/GenEndPoint.cs
+++ Assets/Scripts/GameObjectives/GenEndPoint.cs
@@ -1,34 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.InputSystem;
 
 public class GenEndPoint : MonoBehaviour
 {
-    public GameObject endMarker;
+    public GameObject stickman;
 
-    public GameObject ground;
+    public GameObject arrow;
 
     public GameObject car;
 
-    private Vector3 randomPosition;
+    private Vector3 arrowPosition;
+
+    private Vector3 stickManPosition;
 
     private void Start()
     {
-        Renderer groundRenderer = ground.GetComponent<Renderer>();
-        Bounds groundBounds = groundRenderer.bounds;
+        //GameObject[] openAreaObjects = GameObject.FindObjectsOfType<GameObject>().Where(obj => obj.name.ToLower().Contains("01green") || obj.name.ToLower().Contains("01line") || obj.name.ToLower().Contains("01street")).ToArray();
+        //int randomInt = Random.Range(0, openAreaObjects.Length);
+        //GameObject randomSpawnArea = openAreaObjects[randomInt];
+        //Renderer randomSpawnArea = randomSpawnArea.GetComponent<Renderer>();
+        //Bounds randomSpawnAreaBounds = randomSpawnArea.bounds;
+        //Vector3 spawnPosition = randomSpawnArea.transform.localPosition;
 
-        Renderer endMarkerRenderer = endMarker.GetComponent<MeshRenderer>();
-        Bounds endMarkerBounds = endMarkerRenderer.bounds;
+        Dictionary<int, List<int>> staticXPaths = new Dictionary<int, List<int>>();
+        staticXPaths.Add(450, new List<int> {-1250, 1500});
+        staticXPaths.Add(-330, new List<int> {-1350, 1350});
 
-        randomPosition = new Vector3(
-            Random.Range(groundBounds.min.x, groundBounds.max.x),
-            groundBounds.max.y + 100,
-            Random.Range(groundBounds.min.z, groundBounds.max.z)
-        );
+        Dictionary<int, List<int>> staticZPaths = new Dictionary<int, List<int>>();
+        staticZPaths.Add(20, new List<int> {-1350, 450});
+        staticZPaths.Add(-1510, new List<int> {-250, 700});
 
-        GameObject spawnedObject = Instantiate(endMarker, randomPosition, Quaternion.identity);
+        int xPos;
+        int zPos;
 
-        Debug.Log("Target marker is at: " + randomPosition);
+        if (Random.Range(0, 2) == 0)
+        {
+            KeyValuePair<int, List<int>> randomPair = staticXPaths.ElementAt(UnityEngine.Random.Range(0, staticXPaths.Count));
+            xPos = randomPair.Key;
+            zPos = Random.Range(randomPair.Value.ElementAt(0), randomPair.Value.ElementAt(1));
+        }
+        else
+        {
+            KeyValuePair<int, List<int>> randomPair = staticZPaths.ElementAt(UnityEngine.Random.Range(0, staticZPaths.Count));
+            xPos = Random.Range(randomPair.Value.ElementAt(0), randomPair.Value.ElementAt(1));
+            zPos = randomPair.Key;
+        }
+
+        arrowPosition = new Vector3(xPos, 175, zPos);
+        GameObject arrowObject = Instantiate(arrow, arrowPosition, Quaternion.identity);
+        stickManPosition = new Vector3(xPos, 10, zPos);
+        GameObject stickManObject = Instantiate(stickman, stickManPosition, Quaternion.identity);
+
+        Debug.Log(string.Format("Target is at: {0}", stickManPosition));
     }
 
     private void Update()
