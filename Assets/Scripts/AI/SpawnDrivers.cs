31297ed3ad31786aa665534b6d974c04687b70c1
Kab Thangarajah <kabilanm.thangarajah@gmail.com>
2023-02-19 00:26:21 -0800

    possible to add car types

--- Assets/Scripts/AI/SpawnDrivers.cs
+++ Assets/Scripts/AI/SpawnDrivers.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnDrivers : MonoBehaviour
 {
-    [SerializeField] public GameObject objectToSpawn;
+    public List<GameObject> objectsToSpawn;
     //Vector3 myVector;
     // Start is called before the first frame update
     void Start()
@@ -14,29 +14,35 @@ public class SpawnDrivers : MonoBehaviour
     }
 
     // Update is called once per frame
-    
+
     void Update()
     {
 
     }
 
-    void spawnRoadOne(){
+    void spawnRoadOne()
+    {
         int z1 = -1600;
         int z2 = 805;
         int interval = 100;
-        for(int z = z1; z < z2; z+=interval){
-            Instantiate(objectToSpawn, new Vector3(445,3, z), transform.rotation);
-            Instantiate(objectToSpawn, new Vector3(435,3, z), transform.rotation);
+        for (int z = z1; z < z2; z += interval)
+        {
+            GameObject vehicle = objectsToSpawn[Random.Range(0, objectsToSpawn.Count)];
+            Instantiate(vehicle, new Vector3(445, 3, z), transform.rotation);
+            Instantiate(vehicle, new Vector3(435, 3, z), transform.rotation);
         }
     }
 
-    void spawnRoadTwo(){
+    void spawnRoadTwo()
+    {
         int x1 = -1300;
         int x2 = 1350;
         int interval = 100;
-        for(int x = x1; x < x2; x+=interval){
-            Instantiate(objectToSpawn, new Vector3(x,3, -5), transform.rotation);
-            Instantiate(objectToSpawn, new Vector3(x,3, 8), transform.rotation);
+        for (int x = x1; x < x2; x += interval)
+        {
+            GameObject vehicle = objectsToSpawn[Random.Range(0, objectsToSpawn.Count)];
+            Instantiate(vehicle, new Vector3(x, 3, -5), transform.rotation);
+            Instantiate(vehicle, new Vector3(x, 3, 8), transform.rotation);
         }
     }
 }
